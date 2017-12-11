d1d7f608db4e77feca7ed9f2695e5672d9eb84fb
Anna Boeri <annaboeri@gmail.com>
2017-12-10 17:12:26 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -25,6 +25,7 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function () {
+	console.log("checking for match")
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 	} else {
@@ -32,21 +33,39 @@ var checkForMatch = function () {
 	}
 };
 
-var flipCard = function (cardId){
-
+var flipCard = function (){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
+	this.onload = () => {
+		console.log('image loaded');
+		if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+	};
 
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
+	
+};
 
-	if (cardsInPlay.length === 2){
-		checkForMatch();
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 	}
 };
 
-flipCard(0);
+createBoard();
 
-flipCard(2);
+
+
+
+
+
 
 
