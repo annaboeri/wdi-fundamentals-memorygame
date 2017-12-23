c52066d7d74cb6ed5900a0b6dec5d02366b60b95
Anna Boeri <annaboeri@gmail.com>
2017-12-23 11:28:05 -0800

    added a reset button

--- js/main.js
+++ js/main.js
@@ -33,23 +33,21 @@ var checkForMatch = function () {
 	}
 };
 
+// function that displays the card image when a user clicks on the card
 var flipCard = function (){
 	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
-	this.onload = () => {
-		console.log('image loaded');
 		if (cardsInPlay.length === 2){
 		checkForMatch();
-	}
 	};
 
 	cardsInPlay.push(cards[cardId].rank);
-	this.setAttribute('src', cards[cardId].cardImage)
+	this.setAttribute('src', cards[cardId].cardImage);
 	
 };
-
+//function that loops through cards array and adds a click event to each card, so that when a card is clicked, the flipCard function will run
 var createBoard = function (){
 	for (var i = 0; i < cards.length; i++){
 		var cardElement = document.createElement('img');
@@ -62,8 +60,15 @@ var createBoard = function (){
 
 createBoard();
 
+var resetBoard = function (){
+		var gameBoard = document.getElementById('game-board');
+		while (gameBoard.firstChild) {
+			gameBoard.removeChild(gameBoard.firstChild);
+		}
+		createBoard();
+};
 
-
+document.getElementById("reset").addEventListener("click", resetBoard);
 
 
 
